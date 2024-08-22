93e22bfa52fe941395172464552c31dc6f9cef7e
breike <eike.beyer@yandex.com>
2024-08-22 13:49:32 +0300

    added support for other my keyboards

--- evdev-remapping.py
+++ evdev-remapping.py
@@ -22,72 +22,143 @@ import atexit
 # You need to install evdev with a package manager or pip3.
 import evdev  # (sudo pip3 install evdev)
 
+# set keyboard to look for. Available options: 'akko', 'thinkpad'
+wanted_keyboard = 'thinkpad'
+
 
 # Define an example dictionary describing the remaps.
-REMAP_TABLE = {
-    # Let's swap A and B...
-    evdev.ecodes.KEY_CAPSLOCK: evdev.ecodes.KEY_WWW,
-    evdev.ecodes.KEY_TAB: evdev.ecodes.KEY_ESC,
-    evdev.ecodes.KEY_ESC: evdev.ecodes.KEY_GRAVE,
-    evdev.ecodes.KEY_GRAVE: evdev.ecodes.KEY_TAB,
-    #evdev.ecodes.KEY_SPACE: evdev.ecodes.KEY_MAIL,
-    evdev.ecodes.KEY_LEFTMETA: evdev.ecodes.KEY_LEFTCTRL,
-    evdev.ecodes.KEY_LEFTCTRL: evdev.ecodes.KEY_LEFTMETA,
-    evdev.ecodes.KEY_1: evdev.ecodes.KEY_STOP,
-    evdev.ecodes.KEY_2: evdev.ecodes.KEY_STOP,
-    evdev.ecodes.KEY_3: evdev.ecodes.KEY_STOP,
-    evdev.ecodes.KEY_4: evdev.ecodes.KEY_SPACE,
-    evdev.ecodes.KEY_5: evdev.ecodes.KEY_STOP,
-    evdev.ecodes.KEY_6: evdev.ecodes.KEY_BACKSPACE,
-    evdev.ecodes.KEY_7: evdev.ecodes.KEY_STOP,
-    evdev.ecodes.KEY_8: evdev.ecodes.KEY_STOP,
-    evdev.ecodes.KEY_9: evdev.ecodes.KEY_STOP,
-    evdev.ecodes.KEY_0: evdev.ecodes.KEY_STOP,
-    evdev.ecodes.KEY_MINUS: evdev.ecodes.KEY_SPACE,
-    evdev.ecodes.KEY_EQUAL: evdev.ecodes.KEY_SPACE,
-}
+REMAP_TABLE = {}
+SPACE_KEYS  = {}
+SHIFT_KEYS  = {}
 
-# mapping for space hotkeys (Space+Key, etc...)
-SPACE_KEYS = {
-    # Space+Q to 1
-    evdev.ecodes.KEY_Q: evdev.ecodes.KEY_1,
-    # Space+W to 2
-    evdev.ecodes.KEY_W: evdev.ecodes.KEY_2,
-    # Space+E to 3
-    evdev.ecodes.KEY_E: evdev.ecodes.KEY_3,
-    # Space+R to 4
-    evdev.ecodes.KEY_R: evdev.ecodes.KEY_4,
-    # Space+A to 5
-    evdev.ecodes.KEY_A: evdev.ecodes.KEY_5,
-    # Space+S to 6
-    evdev.ecodes.KEY_S: evdev.ecodes.KEY_6,
-    # Space+D to 7
-    evdev.ecodes.KEY_D: evdev.ecodes.KEY_7,
-    # Space+F to 8
-    evdev.ecodes.KEY_F: evdev.ecodes.KEY_8,
-    # Space+Z to 9
-    evdev.ecodes.KEY_Z: evdev.ecodes.KEY_9,
-    # Space+X to 0
-    evdev.ecodes.KEY_X: evdev.ecodes.KEY_0,
-    # Space+C to minus
-    evdev.ecodes.KEY_C: evdev.ecodes.KEY_MINUS,
-    # Space+V to equal
-    evdev.ecodes.KEY_V: evdev.ecodes.KEY_EQUAL,
-    # Space+Caps Lock to mail key
-    evdev.ecodes.KEY_CAPSLOCK: evdev.ecodes.KEY_EJECTCD,
-}
+if wanted_keyboard == 'akko':
+    REMAP_TABLE = {
+        # Let's swap A and B...
+        evdev.ecodes.KEY_CAPSLOCK: evdev.ecodes.KEY_WWW,
+        evdev.ecodes.KEY_TAB: evdev.ecodes.KEY_ESC,
+        evdev.ecodes.KEY_ESC: evdev.ecodes.KEY_GRAVE,
+        evdev.ecodes.KEY_GRAVE: evdev.ecodes.KEY_TAB,
+        #evdev.ecodes.KEY_SPACE: evdev.ecodes.KEY_MAIL,
+        evdev.ecodes.KEY_LEFTMETA: evdev.ecodes.KEY_LEFTCTRL,
+        evdev.ecodes.KEY_LEFTCTRL: evdev.ecodes.KEY_LEFTMETA,
+        evdev.ecodes.KEY_1: evdev.ecodes.KEY_STOP,
+        evdev.ecodes.KEY_2: evdev.ecodes.KEY_STOP,
+        evdev.ecodes.KEY_3: evdev.ecodes.KEY_STOP,
+        evdev.ecodes.KEY_4: evdev.ecodes.KEY_SPACE,
+        evdev.ecodes.KEY_5: evdev.ecodes.KEY_STOP,
+        evdev.ecodes.KEY_6: evdev.ecodes.KEY_BACKSPACE,
+        evdev.ecodes.KEY_7: evdev.ecodes.KEY_STOP,
+        evdev.ecodes.KEY_8: evdev.ecodes.KEY_STOP,
+        evdev.ecodes.KEY_9: evdev.ecodes.KEY_STOP,
+        evdev.ecodes.KEY_0: evdev.ecodes.KEY_STOP,
+        evdev.ecodes.KEY_MINUS: evdev.ecodes.KEY_SPACE,
+        evdev.ecodes.KEY_EQUAL: evdev.ecodes.KEY_SPACE,
+    }
 
-# mapping for shift hotkeys (Shift+Key, etc...)
-SHIFT_KEYS = {
-    # force space key for shift hotkey (Shift+Space)
-    evdev.ecodes.KEY_SPACE: evdev.ecodes.KEY_SPACE,
-}
+    # mapping for space hotkeys (Space+Key, etc...)
+    SPACE_KEYS = {
+        # Space+Q to 1
+        evdev.ecodes.KEY_Q: evdev.ecodes.KEY_1,
+        # Space+W to 2
+        evdev.ecodes.KEY_W: evdev.ecodes.KEY_2,
+        # Space+E to 3
+        evdev.ecodes.KEY_E: evdev.ecodes.KEY_3,
+        # Space+R to 4
+        evdev.ecodes.KEY_R: evdev.ecodes.KEY_4,
+        # Space+A to 5
+        evdev.ecodes.KEY_A: evdev.ecodes.KEY_5,
+        # Space+S to 6
+        evdev.ecodes.KEY_S: evdev.ecodes.KEY_6,
+        # Space+D to 7
+        evdev.ecodes.KEY_D: evdev.ecodes.KEY_7,
+        # Space+F to 8
+        evdev.ecodes.KEY_F: evdev.ecodes.KEY_8,
+        # Space+Z to 9
+        evdev.ecodes.KEY_Z: evdev.ecodes.KEY_9,
+        # Space+X to 0
+        evdev.ecodes.KEY_X: evdev.ecodes.KEY_0,
+        # Space+C to minus
+        evdev.ecodes.KEY_C: evdev.ecodes.KEY_MINUS,
+        # Space+V to equal
+        evdev.ecodes.KEY_V: evdev.ecodes.KEY_EQUAL,
+        # Space+Caps Lock to mail key
+        evdev.ecodes.KEY_CAPSLOCK: evdev.ecodes.KEY_EJECTCD,
+    }
+
+    # mapping for shift hotkeys (Shift+Key, etc...)
+    SHIFT_KEYS = {
+        # force space key for shift hotkey (Shift+Space)
+        evdev.ecodes.KEY_SPACE: evdev.ecodes.KEY_SPACE,
+    }
+elif wanted_keyboard == 'thinkpad':
+    REMAP_TABLE = {
+        # Let's swap A and B...
+        evdev.ecodes.KEY_CAPSLOCK: evdev.ecodes.KEY_COMPUTER,
+        evdev.ecodes.KEY_TAB: evdev.ecodes.KEY_ESC,
+        evdev.ecodes.KEY_ESC:  evdev.ecodes.KEY_TAB,
+        #evdev.ecodes.KEY_GRAVE: evdev.ecodes.KEY_TAB,
+        #evdev.ecodes.KEY_SPACE: evdev.ecodes.KEY_MAIL,
+        evdev.ecodes.KEY_LEFTMETA: evdev.ecodes.KEY_LEFTCTRL,
+        evdev.ecodes.KEY_LEFTCTRL: evdev.ecodes.KEY_LEFTMETA,
+        evdev.ecodes.KEY_1: evdev.ecodes.KEY_STOP,
+        evdev.ecodes.KEY_2: evdev.ecodes.KEY_STOP,
+        evdev.ecodes.KEY_3: evdev.ecodes.KEY_STOP,
+        evdev.ecodes.KEY_4: evdev.ecodes.KEY_SPACE,
+        evdev.ecodes.KEY_5: evdev.ecodes.KEY_STOP,
+        evdev.ecodes.KEY_6: evdev.ecodes.KEY_BACKSPACE,
+        evdev.ecodes.KEY_7: evdev.ecodes.KEY_STOP,
+        evdev.ecodes.KEY_8: evdev.ecodes.KEY_STOP,
+        evdev.ecodes.KEY_9: evdev.ecodes.KEY_STOP,
+        evdev.ecodes.KEY_0: evdev.ecodes.KEY_STOP,
+        evdev.ecodes.KEY_MINUS: evdev.ecodes.KEY_SPACE,
+        evdev.ecodes.KEY_EQUAL: evdev.ecodes.KEY_SPACE,
+    }
+
+    # mapping for space hotkeys (Space+Key, etc...)
+    SPACE_KEYS = {
+        # Space+Q to 1
+        evdev.ecodes.KEY_Q: evdev.ecodes.KEY_1,
+        # Space+W to 2
+        evdev.ecodes.KEY_W: evdev.ecodes.KEY_2,
+        # Space+E to 3
+        evdev.ecodes.KEY_E: evdev.ecodes.KEY_3,
+        # Space+R to 4
+        evdev.ecodes.KEY_R: evdev.ecodes.KEY_4,
+        # Space+A to 5
+        evdev.ecodes.KEY_A: evdev.ecodes.KEY_5,
+        # Space+S to 6
+        evdev.ecodes.KEY_S: evdev.ecodes.KEY_6,
+        # Space+D to 7
+        evdev.ecodes.KEY_D: evdev.ecodes.KEY_7,
+        # Space+F to 8
+        evdev.ecodes.KEY_F: evdev.ecodes.KEY_8,
+        # Space+Z to 9
+        evdev.ecodes.KEY_Z: evdev.ecodes.KEY_9,
+        # Space+X to 0
+        evdev.ecodes.KEY_X: evdev.ecodes.KEY_0,
+        # Space+C to minus
+        evdev.ecodes.KEY_C: evdev.ecodes.KEY_MINUS,
+        # Space+V to equal
+        evdev.ecodes.KEY_V: evdev.ecodes.KEY_EQUAL,
+        # Space+Caps Lock to mail key
+        evdev.ecodes.KEY_CAPSLOCK: evdev.ecodes.KEY_MAIL,
+    }
+
+    # mapping for shift hotkeys (Shift+Key, etc...)
+    SHIFT_KEYS = {
+        # force space key for shift hotkey (Shift+Space)
+        evdev.ecodes.KEY_SPACE: evdev.ecodes.KEY_SPACE,
+    }
 
 # The names can be found with evtest or in evdev docs.
 
-
 # The keyboard name we will intercept the events for. Obtainable with evtest.
-MATCH = 'Akko 2.4G Wireless Keyboard'
+MATCH = ""
+if wanted_keyboard == 'akko':
+    MATCH = 'Akko 2.4G Wireless Keyboard'
+elif wanted_keyboard == 'thinkpad':
+    MATCH = 'AT Translated Set 2 keyboard'
+
 # Find all input devices.
 devices = [evdev.InputDevice(fn) for fn in evdev.list_devices()]
 # Limit the list to those containing MATCH and pick the first one.
